09cfebcc4da0c1a828fb0b00e05d122918815ccb
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2023-05-20 22:02:39 +0200

    pwa setup

---
+++ pwa.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'Mon entrainement',
+  short_name: 'Mon entrainement',
+  start_url: '/',
+  display: 'standalone',
+  background_color: '#ffffff',
+  theme_color: '#000000',
+  icons: [
+    {
+      src: 'src/assets/workout.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+  ],
+  // Autres options PWA
+};
